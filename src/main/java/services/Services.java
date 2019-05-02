f6841894a509b74a7e1589a623d045daec317543
Gillpoitou <39384177+Gillpoitou@users.noreply.github.com>
2019-05-02 11:41:24 +0200

    [Back Service]Update DB init

--- src/main/java/services/Services.java
+++ src/main/java/services/Services.java
@@ -6,9 +6,19 @@
 
 package services;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.mongodb.client.MongoClient;
 import dao.BusStopDAO;
 import dao.PersonDAO;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Vector;
+import static javax.ws.rs.core.HttpHeaders.USER_AGENT;
 import modele.BusStop;
 import modele.Person;
 
@@ -36,12 +46,55 @@ public class Services {
     }
     
     public static boolean initDataBase(MongoClient mongoClient){
+        System.out.println("stating initialization");
         try{
             BusStopDAO busStopDAO = new BusStopDAO(mongoClient);
-            busStopDAO.selectBusStops(4.863718173086466,45.7708809489496);
+            Vector<BusStop> busStops = busStopDAO.selectBusStops(4.863718173086466,45.7708809489496);
+            float duration = getOSRMdistanceDuration(4.833944353746921,45.74349532570069,4.852895527422981,45.709186530065985);
+            System.out.println(duration);
+            
             return true;
         }catch(Exception e){
+            e.printStackTrace();
             return false;
         }
     }
+    // HTTP GET request
+    private static float getOSRMdistanceDuration(double latA, double longA, double latB, double longB) throws Exception {
+
+                System.out.println("Sending HTTP request");
+		String url = "http://router.project-osrm.org/route/v1/driving/"+latA+","+longA+";" +latB+","+longB+"?overview=false";
+		
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		con.setRequestMethod("GET");
+
+		//add request header
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+		System.out.println(response.toString());
+                
+                JsonElement jelement = new JsonParser().parse(response.toString());
+                JsonObject  racine = jelement.getAsJsonObject();
+                JsonArray route = racine.getAsJsonArray("routes");
+                
+                return route.get(0).getAsJsonObject().get("duration").getAsFloat();
+	}
 }
+
