b94b0e0173357badacc7367be37bc84fe13e3273
elise <elise@LAPTOP-QMVE133S>
2019-05-06 09:36:28 +0200

    [ADD] Converters verified

--- src/main/java/converter/PersonConverter.java
+++ src/main/java/converter/PersonConverter.java
@@ -39,34 +39,51 @@ public class PersonConverter {
         
         return doc;
     }
+     
+     public static Document toConstantDocument(Person person) {
+        Document doc = new Document("arrival", BusStopConverter.toConstantDocument(person.getArrival()))
+                .append("departure", BusStopConverter.toConstantDocument(person.getDeparture()));
+        
+        if (person.getId() != null) {
+            doc.append("_id", new ObjectId(person.getId()));
+        }
+        
+        return doc;
+    }
     
     public static Person toPerson(Document doc) {
-		Person p = new Person();
-		p.setArrival((BusStop) BusStopConverter.toBusStop((Document)doc.get("arrival")));
-		p.setDeparture((BusStop) BusStopConverter.toBusStop((Document)doc.get("departure")));
-                p.setLine((Line) LineConverter.toLine((Document)doc.get("line")));
-                p.setTimeDeparture((Date) doc.get("timeDeparture"));
-		ObjectId id = (ObjectId) doc.get("_id");
-		p.setId(id.toHexString());
-		return p;
+        Person p = new Person();
+        p.setArrival((BusStop) BusStopConverter.toBusStop((Document)doc.get("arrival")));
+        p.setDeparture((BusStop) BusStopConverter.toBusStop((Document)doc.get("departure")));
 
-	}
-  public static Person jsonToPerson(MongoClient mongoClient, String json) throws Exception{
+        if (doc.get("line") != null) {
+            p.setLine((Line) LineConverter.toLine((Document)doc.get("line")));
+        }
+        if (doc.get("timeDeparture") != null) {
+            p.setTimeDeparture((Date) doc.get("timeDeparture"));
+        }
+        ObjectId id = (ObjectId) doc.get("_id");
+        p.setId(id.toHexString());
+        return p;
+
+    }
+    
+    public static Person jsonToPerson(MongoClient mongoClient, String json) throws Exception{
         
-    JsonElement jelement = new JsonParser().parse(json);
-    JsonObject  jsonPerson = jelement.getAsJsonObject();
-    jsonPerson = jsonPerson.getAsJsonObject("person");
-    SimpleDateFormat sdf = new SimpleDateFormat("\"dd-MM-yyyy HH:mm:ss\"");
-    Date date = sdf.parse(jsonPerson.get("departure_date").toString());
+        JsonElement jelement = new JsonParser().parse(json);
+        JsonObject  jsonPerson = jelement.getAsJsonObject();
+        jsonPerson = jsonPerson.getAsJsonObject("person");
+        SimpleDateFormat sdf = new SimpleDateFormat("\"dd-MM-yyyy HH:mm:ss\"");
+        Date date = sdf.parse(jsonPerson.get("departure_date").toString());
     
-    BusStopDAO bsdao = new BusStopDAO(mongoClient);
-    BusStop departure = bsdao.getBusStopById(jsonPerson.get("departure").toString());
-    BusStop arrival = bsdao.getBusStopById(jsonPerson.get("arrival").toString());
-    
-    Person person = new Person();
-    person.setArrival(arrival);
-    person.setDeparture(departure);
-    person.setTimeDeparture(date);
-    return person;
+        BusStopDAO bsdao = new BusStopDAO(mongoClient);
+        BusStop departure = bsdao.getBusStopById(jsonPerson.get("departure").toString());
+        BusStop arrival = bsdao.getBusStopById(jsonPerson.get("arrival").toString());
+
+        Person person = new Person();
+        person.setArrival(arrival);
+        person.setDeparture(departure);
+        person.setTimeDeparture(date);
+        return person;
     }
 }
